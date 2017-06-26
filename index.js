8055e40928adb6facb5135042076cd6127fb65f9
Brandon Semilla <semibran@gmail.com>
2017-06-26 13:07:55 -0700

    optimize 'blocks'

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const states = require('./states')
 
 exports.types = Object.keys(states)
 exports.states = states
-exports.blocks = function blocks(piece) {
-  return states[piece.type][piece.rotation].map(offset)
-  function offset(cell) {
-    return [cell[0] + piece.position[0], cell[1] + piece.position[1]]
+exports.blocks = function (piece) {
+  var position = piece.position
+  var cells = states[piece.type][piece.rotation]
+  var length = cells.length
+  var blocks = new Array(length)
+  for (var i = 0; i < length; i++) {
+    var cell = cells[i]
+    blocks[i] = [cell[0] + position[0], cell[1] + position[1]]
   }
+  return blocks
 }
